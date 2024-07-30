3cfa2b55d5dcb6b485711ec5561b5e677faa958a
andosovmagzum <andosov.magzum@gmail.com>
2024-07-30 16:17:34 +0500

    testing

--- AndosovMagzum/test01.py
+++ AndosovMagzum/test01.py
@@ -1,22 +1,26 @@
 from selenium import webdriver
+from faker import Faker
 from selenium.webdriver.common.by import By
+import time
+
+fake = Faker()
 
 driver = webdriver.Chrome()
-driver.get("https://www.google.com/")
+driver.get("https://qasvus.wordpress.com")
 driver.maximize_window()
 
+assert "California Real Estate" in driver.title
 print(driver.title)
-print(driver.current_url)
 
-driver.find_element(By.XPATH, value="//img[@id='hplogo']")
-driver.find_element(By.XPATH, value="//a[contains(text(),'Почта')]")
+driver.find_element(By.ID, "g2-name").send_keys(fake.name())
 
-GoogleIcon = driver.find_element(By.XPATH, value="//img[@id='hplogo']")
+driver.find_element(By.ID, "g2-email").send_keys(fake.email())
 
-if GoogleIcon:
-    print("Google Logo is OK")
-else:
-    print("No Google Logo")
+driver.find_element(By.ID, "contact-form-comment-g2-message").send_keys(fake.text())
 
+driver.find_element(By.XPATH, "//button[contains(@type,'submit')]").click()
+
+driver.find_element(By.XPATH, "//a[contains(text(),'Go back')]").click()
+
+print("OK")
 driver.quit()
-
