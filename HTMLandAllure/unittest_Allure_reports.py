de131c57c20691108697f0c691cf8e587c866ace
andosovmagzum <andosov.magzum@gmail.com>
2024-09-03 13:47:15 +0500

    testing

--- HTMLandAllure/unittest_Allure_reports.py
+++ HTMLandAllure/unittest_Allure_reports.py
@@ -1,12 +1,11 @@
-# coding=utf8
 # Two browsers test for Google and Wikipedia with Waiting functionality, screenshots and HTML Reports
 import unittest
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 import time
-import HtmlTestRunner
 from selenium.webdriver.common.keys import Keys
+import AllureReports
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -18,13 +17,13 @@ class ChromeSearchTest(unittest.TestCase):
         self.driver.maximize_window()
 
     # Methods in UnitTest should start from "test" keyword
-    def test_search_1(self):
-        driver = self.driver
-        driver.get("http://www.google.com")
 
+    def test_search_weather_chrome(self):
+        driver = self.driver
+        driver.get('http://www.google.com')
         wait = WebDriverWait(driver, 5)
         wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@name='q']")))
-        time.sleep(1)  # simulate long-running test
+        time.sleep(1)  # simulate long running test
 
         search = driver.find_element(By.NAME, "q")
         search.clear()
@@ -34,7 +33,7 @@ class ChromeSearchTest(unittest.TestCase):
 
         # Check if the search returns any result
         assert "No results found." not in driver.page_source, "No results found in Chrome"
-        assert "Weather San Jose" in driver.title
+        assert "Weather San Jose - Google Search" in driver.title
         print("Page title in Chrome is:", driver.title)
 
         # Check Weather frame functionality
@@ -57,13 +56,13 @@ class ChromeSearchTest(unittest.TestCase):
         print("Temperature button is clickable")
         driver.find_element(By.ID, "wob_temp").click()
 
-    def test_search_2(self):
+    def test_search_weather_chrome_1120x850(self):
         driver = self.driver
-        driver.get("http://www.google.com")
-
+        driver.set_window_size(1120, 850)
+        driver.get('http://www.google.com')
         wait = WebDriverWait(driver, 5)
-        wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@name='q']")))
-        time.sleep(1)  # simulate long-running test
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@name='qqq']")))
+        time.sleep(1)
 
         search = driver.find_element(By.NAME, "q")
         search.clear()
@@ -73,32 +72,49 @@ class ChromeSearchTest(unittest.TestCase):
 
         # Check if the search returns any result
         assert "No results found." not in driver.page_source, "No results found in Chrome"
-        assert "Weather San Jose" in driver.title
+        assert "Weather San Jose - Google Search" in driver.title
         print("Page title in Chrome is:", driver.title)
 
         # Check Weather frame functionality
-        wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="wob_wcccc"]')))
-        wait.until(EC.visibility_of_element_located((By.ID, 'wob_rain')))
-        print("Precipitation button is visible")
-        wait.until(EC.element_to_be_clickable((By.ID, 'wob_rain')))
-        print("Precipitation button is clickable")
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="wob_wc"]')))
         driver.find_element(By.ID, "wob_rain").click()
         time.sleep(1)
-        wait.until(EC.visibility_of_element_located((By.ID, 'wob_wind')))
-        print("Wind button is visible")
-        wait.until(EC.element_to_be_clickable((By.ID, 'wob_wind')))
-        print("Wind button is clickable")
         driver.find_element(By.ID, "wob_wind").click()
         time.sleep(1.5)
-        wait.until(EC.visibility_of_element_located((By.ID, 'wob_temp')))
-        print("Temperature button is visible")
-        wait.until(EC.element_to_be_clickable((By.ID, 'wob_temp')))
-        print("Temperature button is clickable")
         driver.find_element(By.ID, "wob_temp").click()
 
+    def test_search_weather_chrome_1120x950(self):
+        driver = self.driver
+        driver.set_window_size(1120, 950)
+        driver.get('http://www.google.com')
+        wait = WebDriverWait(driver, 5)
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@name='q']")))
+        time.sleep(1)
+
+        search = driver.find_element(By.NAME, "q")
+        search.clear()
+        search.send_keys("Weather San Jose")
+        search.submit()
+        time.sleep(1)
+
+        # Check if the search returns any result
+        assert "No results found." not in driver.page_source, "No results found in Chrome"
+        assert "WOW!" in driver.title
+        print("Page title in Chrome is:", driver.title)
+
+        # Check Weather frame functionality
+        wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="wob_wc"]')))
+        driver.find_element(By.ID, "wob_rain").click()
+        time.sleep(1)
+        driver.find_element(By.ID, "wob_wind").click()
+        time.sleep(1.5)
+        driver.find_element(By.ID, "wob_temp").click()
+
+    # Anything declared in tearDown will be executed for all test cases
+    # Closing browser. You need to use "tearDown" method only one time for every Class
     def tearDown(self):
         self.driver.quit()
 
 
-if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='./HtmlReports'))
+if __name__ == "__main__":
+    unittest.main(AllureReports)
